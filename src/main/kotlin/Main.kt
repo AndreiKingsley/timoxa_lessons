067fe2017b0f1d8a5b2790a9fc01e0513b4c46ba
Andrei <kisandy@yandex.ru>
2022-08-29 22:15:13 +0400

    fix

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,61 @@
+/**
+ * Operator can be one of the following: '+', '-', '*'
+ */
+fun evaluate(leftOperand: Int, rightOperand: Int, operator: Char): Int {
+    TODO()
+}
+
+/**
+ * Returns 0 if array is empty.
+ */
+fun sumAll(values: Array<Int>): Int {
+    // use `for`!!!
+    TODO()
+}
+
+/**
+ * Case sensitive!
+ */
+fun isPalindrome(string: String): Boolean{
+    TODO()
+}
+
+fun main(args: Array<String>) {
+
+    // 1.evaluate
+
+
+    // 1 + 5
+    // 6 * 7
+    // 8 * 9
+    /*
+    val expressionParts = readln().split(" ")
+    val leftOperand = expressionParts[0].toInt()
+    val operator = expressionParts[1].first()
+    val rightOperand = expressionParts[2].toInt()
+    println(evaluate(leftOperand, rightOperand, operator))
+
+
+
+     */
+
+    // 2. isPalindrome
+
+    /*
+    val input = readln()
+    println(isPalindrome(input))
+
+     */
+
+
+
+
+    // 3. sumAll
+    /*
+    val numbers = readln().split(" ").map { it.toInt() }.toTypedArray()
+    println(sumAll(numbers))
+
+
+     */
+
+}
